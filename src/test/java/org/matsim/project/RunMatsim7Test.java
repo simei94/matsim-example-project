c2894e956e1a229f32fedcf714a5695d9a88f1ab
simei94 <simonmeinhardt94@gmail.com>
2021-11-29 14:26:38 +0100

    new test class

---
+++ src/test/java/org/matsim/project/RunMatsim7Test.java
@@ -0,0 +1,15 @@
+package org.matsim.project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RunMatsim7Test {
+
+    @Test
+    public void main() {
+        Assert.fail("failing");
+
+    }
+}
